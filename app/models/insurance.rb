fe0708510c5e30b90560837f633356741c013f8c
Elliot Cohen <elliot.cohen@gmail.com>
2012-10-28 09:57:59 -0400

    updating syntax to rails 3.2

--- app/models/insurance.rb
+++ app/models/insurance.rb
@@ -2,13 +2,13 @@ class Insurance < ActiveRecord::Base
   belongs_to :user
   has_attached_file :front_photo, 
     :storage => :s3,
-    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+    :s3_credentials => "#{Rails.root}/config/s3.yml",
     :path => ":attachment/:id/:style.:extension",
     :bucket => 'minda_insurance_images'
   
   has_attached_file :back_photo,
     :storage => :s3,
-    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+    :s3_credentials => "#{Rails.root}/config/s3.yml",
     :path => ":attachment/:id/:style.:extension",
     :bucket => 'minda_insurance_images'
     
