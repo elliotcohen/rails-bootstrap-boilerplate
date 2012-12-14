91771e842bc214d8bbab8b639c99ece30956e16c
Elliot Cohen <elliot.cohen@gmail.com>
2012-12-13 20:35:44 -0500

    adding before pushing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,7 @@
 class HomeController < ApplicationController
 
-  def index
-    if user_signed_in?
-      redirect_to '/dashboard/show'
-    end
+  def index      
+    @user = User.new  
   end
 
 end
